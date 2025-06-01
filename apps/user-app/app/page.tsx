76945f452fa74d3df9a0ddba058a077d20716620
parth <parthjadhao4@gmail.com>
2025-06-01 19:45:10 +0530

    chore: initialize monorepo structure and database setup

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { PrismaClient } from "@payment-wallet-app/db/client";
+
+export default function Page() {
+  return ( <div className="text-amber-400">
+    hello
+  </div> );
+}
---
+++ apps/user-app/postcss.config.js
@@ -0,0 +1,3 @@
+import { postcssConfig } from "@payment-wallet-app/tailwind-config/postcss";
+
+export default postcssConfig;
